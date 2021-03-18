add6100256c982ba7526da6d182f0a7aa1e5606e
choudharypooja <79826970+choudharypooja@users.noreply.github.com>
2021-03-18 20:43:10 +0530

    Update policyAssignmentTemplate.ps1

--- policyAssignmentTemplate.ps1
+++ policyAssignmentTemplate.ps1
@@ -1,28 +1,6 @@
-$eventhubNameSpace = "lm-logs-lmpoojachoudhary-westeurope"
-$targetResourceGroup= "lm-pooja-we"
-$eventHubNamespaceId = Get-AzEventHubNamespace -ResourceGroupName $targetresourceGroup -NamespaceName $eventhubNameSpace
+$targetResourceGroup= 'lm-pooja-we'
+$resource = Get-AzResourceGroup -Name $targetResourceGroup
 
-# $eventHubId = Get-AzEventHub -ResourceGroupName $targetresourceGroup -NamespaceName $eventhubNameSpace -EventHubName $eventhubName
-
-# $eventHubAuthorizationIdParam = Get-AzEventHubAuthorizationRule -ResourceGroupName $targetresourceGroup -NamespaceName $eventhubNameSpace -Name $eventhubAuthorizationId
-
-# $azureRegionParam= @{'azureRegions'=($location)}
-
-
-# $eventHubParam = @{'eventHubName'=($eventHubId.Id);'eventHubRuleId'=($eventhubAuthorizationIdParam.Id);'azureRegions'=(-split $location);'profileName'=($resourceGroup);'metricsEnabled'=('True')}
-# $resource = Get-AzResourceGroup -Name $resourceGroup
-
-Write-Output $eventHubNamespaceId
+Write-Output $resource.ResourceId
 $DeploymentScriptOutputs = @{}
-$DeploymentScriptOutputs['text'] = $eventHubNamespaceId
-
-# $eachResource = $resource.ResourceId
-
-# $eachAssignment = @{}
-# $assignment = New-AzPolicyAssignment -Name $resourceGroup -DisplayName $resourceGroup  -PolicySetDefinition $definition -Location $location -PolicyParameterObject  $eventHubParam -AssignIdentity
-
-
-# Start-Sleep -s 15
-# New-AzRoleAssignment -Scope $eachResource -ObjectId $assignment.Identity.PrincipalId  -RoleDefinitionName Contributor
-# $eachAssignment.add($assignment.PolicyAssignmentId,$assignment.ResourceGroupName)
-# return $eachAssignment
+$DeploymentScriptOutputs['resourceId'] = $resource.ResourceId
