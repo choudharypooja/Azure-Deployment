470976fbda0eac40a74edfa43e5af43bdb724f4e
choudharypooja <79826970+choudharypooja@users.noreply.github.com>
2021-03-18 20:09:10 +0530

    Update policyAssignmentTemplate.ps1

--- policyAssignmentTemplate.ps1
+++ policyAssignmentTemplate.ps1
@@ -7,21 +7,21 @@ $targetResourceGroup= "lm-pooja-we"
 
 $definition = Get-AzPolicySetDefinition | Where-Object { $_.Properties.DisplayName -eq 'Azure Diagnostics Policy Initiative to LM' }
 
-$eventHubNamespaceId = Get-AzEventHubNamespace -ResourceGroupName $targetresourceGroup -NamespaceName $eventhubNameSpace
+# $eventHubNamespaceId = Get-AzEventHubNamespace -ResourceGroupName $targetresourceGroup -NamespaceName $eventhubNameSpace
 
-$eventHubId = Get-AzEventHub -ResourceGroupName $targetresourceGroup -NamespaceName $eventhubNameSpace -EventHubName $eventhubName
+# $eventHubId = Get-AzEventHub -ResourceGroupName $targetresourceGroup -NamespaceName $eventhubNameSpace -EventHubName $eventhubName
 
-$eventHubAuthorizationIdParam = Get-AzEventHubAuthorizationRule -ResourceGroupName $targetresourceGroup -NamespaceName $eventhubNameSpace -Name $eventhubAuthorizationId
+# $eventHubAuthorizationIdParam = Get-AzEventHubAuthorizationRule -ResourceGroupName $targetresourceGroup -NamespaceName $eventhubNameSpace -Name $eventhubAuthorizationId
 
-$azureRegionParam= @{'azureRegions'=($location)}
+# $azureRegionParam= @{'azureRegions'=($location)}
 
 
-$eventHubParam = @{'eventHubName'=($eventHubId.Id);'eventHubRuleId'=($eventhubAuthorizationIdParam.Id);'azureRegions'=(-split $location);'profileName'=($resourceGroup);'metricsEnabled'=('True')}
-$resource = Get-AzResourceGroup -Name $resourceGroup
+# $eventHubParam = @{'eventHubName'=($eventHubId.Id);'eventHubRuleId'=($eventhubAuthorizationIdParam.Id);'azureRegions'=(-split $location);'profileName'=($resourceGroup);'metricsEnabled'=('True')}
+# $resource = Get-AzResourceGroup -Name $resourceGroup
 
 Write-Output $output
 $DeploymentScriptOutputs = @{}
-$DeploymentScriptOutputs['text'] = $resource.ResourceId
+$DeploymentScriptOutputs['text'] = $definition
 
 # $eachResource = $resource.ResourceId
 
