5e7505d7935090cde82f657a7de4a695bc2943ff
choudharypooja <79826970+choudharypooja@users.noreply.github.com>
2021-03-18 18:02:15 +0530

    Create policyAssignmentTemplate.ps1

---
+++ policyAssignmentTemplate.ps1
@@ -0,0 +1,31 @@
+$resourceGroup = "lm-pooja-we"
+$location="westeurope"
+$eventhubName = "log-hub"
+$eventhubNameSpace = "lm-logs-lmpoojachoudhary-westeurope"
+$eventhubAuthorizationId = "RootManageSharedAccessKey"
+$targetResourceGroup= "lm-pooja-we"
+
+$definition = Get-AzPolicySetDefinition | Where-Object { $_.Properties.DisplayName -eq 'Azure Diagnostics Policy Initiative to LM' }
+
+$eventHubNamespaceId = Get-AzEventHubNamespace -ResourceGroupName $targetresourceGroup -NamespaceName $eventhubNameSpace
+
+$eventHubId = Get-AzEventHub -ResourceGroupName $targetresourceGroup -NamespaceName $eventhubNameSpace -EventHubName $eventhubName
+
+$eventHubAuthorizationIdParam = Get-AzEventHubAuthorizationRule -ResourceGroupName $targetresourceGroup -NamespaceName $eventhubNameSpace -Name $eventhubAuthorizationId
+
+$azureRegionParam= @{'azureRegions'=($location)}
+
+
+$eventHubParam = @{'eventHubName'=($eventHubId.Id);'eventHubRuleId'=($eventhubAuthorizationIdParam.Id);'azureRegions'=(-split $location);'profileName'=($resourceGroup);'metricsEnabled'=('True')}
+$resource = Get-AzResourceGroup -Name $resourceGroup
+
+$eachResource = $resource.ResourceId
+
+$eachAssignment = @{}
+$assignment = New-AzPolicyAssignment -Name $resourceGroup -DisplayName $resourceGroup -Scope $eachResource  -PolicySetDefinition $definition -Location $location -PolicyParameterObject  $eventHubParam -AssignIdentity
+
+
+Start-Sleep -s 15
+New-AzRoleAssignment -Scope $eachResource -ObjectId $assignment.Identity.PrincipalId  -RoleDefinitionName Contributor
+$eachAssignment.add($assignment.PolicyAssignmentId,$assignment.ResourceGroupName)
+return $eachAssignment
