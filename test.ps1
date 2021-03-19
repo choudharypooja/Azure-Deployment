df4c86491826e63c027a0d48c7f2855c0a4f67ad
choudharypooja <79826970+choudharypooja@users.noreply.github.com>
2021-03-19 14:50:01 +0530

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,15 @@
+param
+(
+	# An interval in seconds to check that trigger was successful
+    [Parameter(Mandatory = $True)]
+    [string[]]$resourceGroups,
+)
+
+$assignments = @()
+foreach ($resourceGroup in $resourceGroups){
+  $assignments += $resourceGroup
+}
+
+Write-Output $assignments
+$DeploymentScriptOutputs = @{}
+$DeploymentScriptOutputs['assignments'] = $assignments
