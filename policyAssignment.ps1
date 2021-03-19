eaf2779720f09f5465e18ea6da40426eb7b92075
choudharypooja <79826970+choudharypooja@users.noreply.github.com>
2021-03-19 22:46:59 +0530

    Update policyAssignment.ps1

--- policyAssignment.ps1
+++ policyAssignment.ps1
@@ -20,26 +20,6 @@ param
 	[string]$targetResourceGroup
 )
 
-function NewAzureRmRoleAssignment($resource, $assignment, $retryCount) {
-    $totalRetries = $retryCount
-    While ($True) {
-        Try {
-            $Null = New-AzRoleAssignment -ObjectId $assignment.Identity.PrincipalId  -RoleDefinitionName Contributor
-            Return
-        }
-        Catch {
-            # The principal could not be found. Maybe it was just created.
-            If ($retryCount -eq 0) {
-                Write-Error "An error occurred: $($_.Exception)`n$($_.ScriptStackTrace)"
-                throw "The principal '$assignment.Identity.PrincipalId' cannot be granted 'Contributor' role on the resource group '$resource.ResourceId'. Please make sure the principal exists and try again later."
-            }
-            $retryCount--
-            Write-Warning "  The principal '$assignment.Identity.PrincipalId' cannot be granted 'Contributor' role on the resource group '$resource.ResourceId'. Trying again (attempt $($totalRetries - $retryCount)/$totalRetries)"
-            Start-Sleep 40
-        }
-    }
-}
-
 $definition = Get-AzPolicySetDefinition | Where-Object { $_.Properties.DisplayName -eq 'Azure Diagnostics Policy Initiative to LM' }
 
 $eventHubNamespaceId = Get-AzEventHubNamespace -ResourceGroupName $targetresourceGroup -NamespaceName $eventhubNameSpace
@@ -57,6 +37,5 @@ $eachAssignment = @{}
 $assignment = New-AzPolicyAssignment -Name $resourceGroup -DisplayName $resourceGroup -Scope $resource.ResourceId  -PolicySetDefinition $definition -Location $location -PolicyParameterObject  $eventHubParam -AssignIdentity
 
 #Start-Sleep -s 15
-NewAzureRmRoleAssignment $resource $assignment 5
 $eachAssignment.add($assignment.PolicyAssignmentId,$assignment.ResourceGroupName)
 return $eachAssignment 
