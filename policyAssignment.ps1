97d40139137a48f1f3816de723c912ad0753d831
choudharypooja <79826970+choudharypooja@users.noreply.github.com>
2021-03-19 21:45:40 +0530

    Update policyAssignment.ps1

--- policyAssignment.ps1
+++ policyAssignment.ps1
@@ -20,6 +20,26 @@ param
 	[string]$targetResourceGroup
 )
 
+function NewAzureRmRoleAssignment($resource, $assignment, $retryCount) {
+    $totalRetries = $retryCount
+    While ($True) {
+        Try {
+            $Null = New-AzRoleAssignment -Scope $resource.ResourceId -ObjectId $assignment.Identity.PrincipalId  -RoleDefinitionName Contributor
+            Return
+        }
+        Catch {
+            # The principal could not be found. Maybe it was just created.
+            If ($retryCount -eq 0) {
+                Write-Error "An error occurred: $($_.Exception)`n$($_.ScriptStackTrace)"
+                throw "The principal '$assignment.Identity.PrincipalId' cannot be granted 'Contributor' role on the resource group '$resource.ResourceId'. Please make sure the principal exists and try again later."
+            }
+            $retryCount--
+            Write-Warning "  The principal '$assignment.Identity.PrincipalId' cannot be granted 'Contributor' role on the resource group '$resource.ResourceId'. Trying again (attempt $($totalRetries - $retryCount)/$totalRetries)"
+            Start-Sleep 20
+        }
+    }
+}
+
 $definition = Get-AzPolicySetDefinition | Where-Object { $_.Properties.DisplayName -eq 'Azure Diagnostics Policy Initiative to LM' }
 
 $eventHubNamespaceId = Get-AzEventHubNamespace -ResourceGroupName $targetresourceGroup -NamespaceName $eventhubNameSpace
@@ -36,7 +56,7 @@ $resource = Get-AzResourceGroup -Name $resourceGroup
 $eachAssignment = @{}
 $assignment = New-AzPolicyAssignment -Name $resourceGroup -DisplayName $resourceGroup -Scope $resource.ResourceId  -PolicySetDefinition $definition -Location $location -PolicyParameterObject  $eventHubParam -AssignIdentity
 
-Start-Sleep -s 15
-New-AzRoleAssignment -Scope $resource.ResourceId -ObjectId $assignment.Identity.PrincipalId  -RoleDefinitionName Contributor
+#Start-Sleep -s 15
+NewAzureRmRoleAssignment $resource $assignment 3
 $eachAssignment.add($assignment.PolicyAssignmentId,$assignment.ResourceGroupName)
 return $eachAssignment 
