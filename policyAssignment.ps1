cd622e5d4a74442a18d0b92ddaeceb137e1731fe
choudharypooja <79826970+choudharypooja@users.noreply.github.com>
2021-03-22 18:40:37 +0530

    Update policyAssignment.ps1

--- policyAssignment.ps1
+++ policyAssignment.ps1
@@ -17,8 +17,46 @@ param
 	[string]$eventhubAuthorizationId,
 
 	[Parameter(Mandatory =$True)]
-	[string]$targetResourceGroup
+	[string]$targetResourceGroup,
+	
+	[Parameter(Mandatory =$True)]
+	[string]$targetResourceGroup,
+	
+	[Parameter(Mandatory=$false)]
+    	[ValidateSet("AzureChinaCloud","AzureCloud","AzureGermanCloud","AzureUSGovernment")]
+    	[string]$Environment = "AzureCloud
 )
+try
+{
+    $AzureLogin = Get-AzSubscription
+    $currentContext = Get-AzContext
+
+    # Establish REST Token
+    $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+    $profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($azProfile)
+    $token = $profileClient.AcquireAccessToken($currentContext.Subscription.TenantId)
+}
+catch
+{
+    $null = Login-AzAccount -Environment $Environment
+    $AzureLogin = Get-AzSubscription
+    $currentContext = Get-AzContext
+    
+    # Establish REST Token
+    $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+    $profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($azProfile)
+    $token = $profileClient.AcquireAccessToken($currentContext.Subscription.TenantId)
+}
+
+Try
+{
+    $Subscription = Get-AzSubscription -SubscriptionId $subscriptionId
+}
+catch
+{
+    Write-Host "Subscription not found"
+    break
+}
 
 $definition = Get-AzPolicySetDefinition | Where-Object { $_.Properties.DisplayName -eq 'Azure Diagnostics Policy Initiative to LM' }
 
