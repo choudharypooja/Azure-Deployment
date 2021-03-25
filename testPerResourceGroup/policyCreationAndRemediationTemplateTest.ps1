6507da846f259b2cfe2674e5c9cea48971e68bc5
Pooja Choudhary <pooja.choudhary@logicmonitor.com>
2021-03-25 13:58:22 +0530

    main:  test

---
+++ testPerResourceGroup/policyCreationAndRemediationTemplateTest.ps1
@@ -0,0 +1,58 @@
+param
+(
+    [Parameter(Mandatory = $True)]
+    [string]$resourceGroup,
+
+    [Parameter(Mandatory =$True)]
+    [string]$lmCompanyName,
+
+    [Parameter(Mandatory =$True)]
+    [string]$subscriptionId,
+
+    [Parameter(Mandatory = $True)]
+    [string]$location
+
+)
+
+Write-Host $resourceGroup
+Write-Host $location
+If($ADO){write-host "ADO switch deprecated and no longer necessary" -ForegroundColor Yellow}
+Write-Host "Authenticating to Azure..." -ForegroundColor Cyan
+
+$Environment = "Azure Cloud"
+# try
+# {
+#     $AzureLogin = Get-AzSubscription
+#     $currentContext = Get-AzContext
+#
+#     # Establish REST Token
+#     $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+#     $profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($azProfile)
+#     $token = $profileClient.AcquireAccessToken($currentContext.Subscription.TenantId)
+# }
+# catch
+# {
+#     $null = Login-AzAccount -Environment $Environment
+#     $AzureLogin = Get-AzSubscription
+#     $currentContext = Get-AzContext
+#
+#     # Establish REST Token
+#     $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+#     $profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($azProfile)
+#     $token = $profileClient.AcquireAccessToken($currentContext.Subscription.TenantId)
+# }
+#
+# Try
+# {
+#     $Subscription = Get-AzSubscription -SubscriptionId $subscriptionId
+# }
+# catch
+# {
+#     Write-Host "Subscription not found"
+#     break
+# }
+#
+# $targetResourceGroup = 'lm-logs-' + $lmCompanyName + '-' + $location + '-group'
+# $eventhubNameSpace = $targetResourceGroup.replace('-group','')
+# $eventhubName = 'log-hub'
+# $eventhubAuthorizationId = 'RootManageSharedAccessKey'
