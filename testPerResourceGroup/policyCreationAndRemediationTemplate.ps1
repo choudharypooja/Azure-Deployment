f75d4a46d482c09e2fc47ab868eadd18e90863a0
Pooja Choudhary <pooja.choudhary@logicmonitor.com>
2021-03-25 13:42:13 +0530

    main:  minor change

--- testPerResourceGroup/policyCreationAndRemediationTemplate.ps1
+++ testPerResourceGroup/policyCreationAndRemediationTemplate.ps1
@@ -14,6 +14,44 @@ param
 
 )
 
+Write-Host $resourceGroup
+Write-Host $location
+If($ADO){write-host "ADO switch deprecated and no longer necessary" -ForegroundColor Yellow}
+Write-Host "Authenticating to Azure..." -ForegroundColor Cyan
+
+$Environment = "Azure Cloud"
+try
+{
+    $AzureLogin = Get-AzSubscription
+    $currentContext = Get-AzContext
+
+    # Establish REST Token
+    $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+    $profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($azProfile)
+    $token = $profileClient.AcquireAccessToken($currentContext.Subscription.TenantId)
+}
+catch
+{
+    $null = Login-AzAccount -Environment $Environment
+    $AzureLogin = Get-AzSubscription
+    $currentContext = Get-AzContext
+
+    # Establish REST Token
+    $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+    $profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($azProfile)
+    $token = $profileClient.AcquireAccessToken($currentContext.Subscription.TenantId)
+}
+
+Try
+{
+    $Subscription = Get-AzSubscription -SubscriptionId $subscriptionId
+}
+catch
+{
+    Write-Host "Subscription not found"
+    break
+}
+
 $targetResourceGroup = 'lm-logs-' + $lmCompanyName + '-' + $location + '-group'
 $eventhubNameSpace = $targetResourceGroup.replace('-group','')
 $eventhubName = 'log-hub'
