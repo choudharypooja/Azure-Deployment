c1dd52189028e0d73c5b7fda0808e303dc3fa4ed
Pooja Choudhary <79826970+choudharypooja@users.noreply.github.com>
2021-08-25 19:32:29 +0530

    Update policyCreationAndRemediationTemplate.ps1

--- policyEvaluationTest/policyCreationAndRemediationTemplate.ps1
+++ policyEvaluationTest/policyCreationAndRemediationTemplate.ps1
@@ -1,10 +1,7 @@
 param
 (
     [Parameter(Mandatory = $True)]
-    [string[]]$resourceGroups,
-
-    [Parameter(Mandatory = $True)]
-    [string]$location,
+    [string]$resourceGroup,
 
     [Parameter(Mandatory =$True)]
     [string]$lmCompanyName,
@@ -12,30 +9,20 @@ param
     [Parameter(Mandatory =$True)]
     [string]$subscriptionId,
 
-    [Parameter(Mandatory=$false)]
-    [ValidateSet("AzureChinaCloud","AzureCloud","AzureGermanCloud","AzureUSGovernment")]
-    [string]$Environment = "AzureCloud"
+    [Parameter(Mandatory = $True)]
+    [string]$location,
+
+    [Parameter(Mandatory = $True)]
+    [string]$sourceCodeBranch
 
 )
 
-$location = $location.replace(' ','').toLower()
-$locationValidity=$True
-$targetResourceGroup = 'lm-logs-' + $lmCompanyName + '-' + $location + '-group'
-$eventhubNameSpace = $targetResourceGroup.replace('-group','')
-$eventhubName = 'log-hub'
-$eventhubAuthorizationId = 'RootManageSharedAccessKey'
-$subsId = $subscriptionId
-foreach ($resourceGroup in $resourceGroups){
-	$resourceGroupDetails = Get-AzResourceGroup -Name $resourceGroup
-	if($location -ne $resourceGroupDetails.Location.replace(' ','').toLower()){
-		Write-Host "The source resource groups are not in the same region...."
-		$locationValidity=$False
-		break
-	}
-}
-
+Write-Host $resourceGroup
+Write-Host $location
 If($ADO){write-host "ADO switch deprecated and no longer necessary" -ForegroundColor Yellow}
 Write-Host "Authenticating to Azure..." -ForegroundColor Cyan
+
+$Environment = "Azure Cloud"
 try
 {
     $AzureLogin = Get-AzSubscription
@@ -68,74 +55,24 @@ catch
     break
 }
 
-If($AzureLogin -and !($SubscriptionID))
-{
-    [array]$SubscriptionArray = Add-IndexNumberToArray (Get-AzSubscription)
-    [int]$SelectedSub = 0
-
-    # use the current subscription if there is only one subscription available
-    if ($SubscriptionArray.Count -eq 1)
-    {
-        $SelectedSub = 1
-    }
-    # Get SubscriptionID if one isn't provided
-    while($SelectedSub -gt $SubscriptionArray.Count -or $SelectedSub -lt 1)
-    {
-        Write-host "Please select a subscription from the list below"
-        $SubscriptionArray | Select-Object "#", Id, Name | Format-Table
-        try
-        {
-            $SelectedSub = Read-Host "Please enter a selection from 1 to $($SubscriptionArray.count)"
-        }
-        catch
-        {
-            Write-Warning -Message 'Invalid option, please try again.'
-        }
-    }
-    if($($SubscriptionArray[$SelectedSub - 1].Name))
-    {
-        $SubscriptionName = $($SubscriptionArray[$SelectedSub - 1].Name)
-    }
-    elseif($($SubscriptionArray[$SelectedSub - 1].SubscriptionName))
-    {
-        $SubscriptionName = $($SubscriptionArray[$SelectedSub - 1].SubscriptionName)
-    }
-    write-verbose "You Selected Azure Subscription: $SubscriptionName"
-
-    if($($SubscriptionArray[$SelectedSub - 1].SubscriptionID))
-    {
-        [guid]$SubscriptionID = $($SubscriptionArray[$SelectedSub - 1].SubscriptionID)
-    }
-    if($($SubscriptionArray[$SelectedSub - 1].ID))
-    {
-        [guid]$SubscriptionID = $($SubscriptionArray[$SelectedSub - 1].ID)
-    }
-}
-Write-Host "Selecting Azure Subscription: $SubscriptionID ..." -ForegroundColor Cyan
-$Null = Select-AzSubscription -SubscriptionId $SubscriptionID
+$targetResourceGroup = 'lm-logs-' + $lmCompanyName + '-' + $location + '-group'
+$eventhubNameSpace = $targetResourceGroup.replace('-group','')
+$eventhubName = 'log-hub'
+$eventhubAuthorizationId = 'RootManageSharedAccessKey'
+$templateUri = 'https://raw.githubusercontent.com/logicmonitor/lm-logs-azure/'+$sourceCodeBranch+'/arm-template-deployment/ARMTemplateExport.json'
 
 Get-AzResourceGroup -Name $targetResourceGroup -ErrorVariable notPresent -ErrorAction SilentlyContinue
-
-if ($notPresent)
-{
-    Write-Host "The target resource group is not present in the specified location or the lm_company_name is incorrect"
+if ($notPresent) {
+    Write-Host "target resource group not present..."
+    exit;
 }
-else
-{
-if($locationValidity){
-New-AzDeployment -Name "Initiative-LM-$location" -TemplateUri "https://raw.githubusercontent.com/choudharypooja/Azure-Deployment/main/ARMTemplateExportTest.json" -Location $location -Verbose
-
-foreach ($resourceGroup in $resourceGroups){
-	$policyAssignments = ./policyAssignment.ps1 -resourceGroup $resourceGroup -location $location -eventhubName $eventhubName -eventhubNameSpace $eventhubNameSpace -eventhubAuthorizationId $eventhubAuthorizationId -targetResourceGroup $targetResourceGroup
-	Write-Host "Runnning compliance result for $($policyAssignments.PolicyAssignmentId)" -ForegroundColor Cyan
-    ./Trigger-PolicyEvaluation.ps1 -SubscriptionId $subsId -ResourceGroup $policyAssignments.ResourceGroupName
-	Start-Sleep -s 30
-	$Null = New-AzRoleAssignment -ObjectId $policyAssignments.Identity.principalId  -RoleDefinitionName Contributor
-	Start-Sleep -s 20
-	./Trigger-PolicyInitiativeRemediation.ps1 -force -SubscriptionId $subsId -PolicyAssignmentId $policyAssignments.PolicyAssignmentId -ResourceGroupName $policyAssignments.ResourceGroupName
-
-}
-}else{
-		Write-Host "Exiting the script...."
-	}
+else{
+    New-AzDeployment -TemplateUri $templateUri -Location "West US" -Verbose
+    $policyAssignments = ./policyAssignment.ps1 -resourceGroup $resourceGroup -location $location -eventhubName $eventhubName -eventhubNameSpace $eventhubNameSpace -eventhubAuthorizationId $eventhubAuthorizationId -targetResourceGroup $targetResourceGroup
+    Write-Host "Running compliance result for $($policyAssignments.PolicyAssignmentId)" -ForegroundColor Cyan
+    Start-AzPolicyComplianceScan -ResourceGroupName $policyAssignments.ResourceGroupName
+    Start-Sleep -s 30
+    $Null = New-AzRoleAssignment -ObjectId $policyAssignments.Identity.principalId  -RoleDefinitionName Contributor
+    Start-Sleep -s 20
+    ./policyInitiativeRemediation.ps1 -force -SubscriptionId $subscriptionId -PolicyAssignmentId $policyAssignments.PolicyAssignmentId -ResourceGroupName $policyAssignments.ResourceGroupName
 }
